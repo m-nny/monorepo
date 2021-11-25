82c94a6917e294891a7709fb5c70ae3f4eda448c
Alibek Manabayev <m-nny@yandex.ru>
2021-11-25 07:45:34 +0600

    init

---
+++ apps/comic-info-generator/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'comic-info-generator',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/comic-info-generator',
+};
